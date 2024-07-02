c5d4ec7990c61d032bf1d1826fcccb8e5ff74980
小易 <a8052@qq.com>
2024-07-02 16:38:26 +0800

    修复安装包镜像源问题

--- YiAiQuickDeploy/public/admin/assets/index-34944812.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * 由 Fantastic-admin 提供技术支持
- * Powered by Fantastic-admin
- * Gitee  https://gitee.com/hooray/fantastic-admin
- * Github https://github.com/hooray/fantastic-admin
- */
-  
-import{d as i,y as m,r as c,B as g,o,I as p,f,c as l,b as e,J as r,t as h,G as k,h as y,n as x}from"./index-e2ea765f.js";const B="/admin/assets/logo-71242da6.png",b=["src"],w={key:1},C=i({name:"Logo"}),L=i({...C,props:{showLogo:{type:Boolean,default:!0},showTitle:{type:Boolean,default:!0}},setup(s){const n=m(),a=c("Yi-Ai"),_=c(B),u=g(()=>{const t={};return n.settings.home.enable&&(t.name="home"),t});return(t,v)=>{const d=y("router-link");return o(),p(d,{to:e(u),class:k(["title",{"is-link":e(n).settings.home.enable}]),title:e(a)},{default:f(()=>[s.showLogo?(o(),l("img",{key:0,src:e(_),class:"logo"},null,8,b)):r("",!0),s.showTitle?(o(),l("span",w,h(e(a)),1)):r("",!0)]),_:1},8,["to","class","title"])}}});const I=x(L,[["__scopeId","data-v-3c2d5fb6"]]);export{I as default};
--- YiAiQuickDeploy/public/admin/assets/index-4e2cd60d.js
+++
@@ -1,9 +0,0 @@
-
-/**
- * 由 Fantastic-admin 提供技术支持
- * Powered by Fantastic-admin
- * Gitee  https://gitee.com/hooray/fantastic-admin
- * Github https://github.com/hooray/fantastic-admin
- */
-  
-import{d as c,r as d,y as g,x as h,o as a,c as n,b as s,t as u,J as l,p as y,i as f,a as r,g as m,n as S}from"./index-e2ea765f.js";import{a as x}from"./config-8ef4521d.js";const i=t=>(y("data-v-4d63aa86"),t=t(),f(),t),C={class:"copyright"},v=i(()=>r("span",null,"Copyright",-1)),k=i(()=>r("span",{class:"icon"},"©",-1)),I=["href"],b=c({name:"Copyright"}),B=c({...b,setup(t){const e=d({copyrightTitle:"NineAi Admin",copyrightUrl:"/"});async function _(){const o=await x.copyright();o.success&&(e.value=o.data)}m();const p=g();return h(()=>{_()}),(o,N)=>(a(),n("footer",C,[v,k,s(p).settings.copyright.beian?(a(),n("a",{key:0,href:s(e).copyrightUrl,target:"_blank",rel:"noopener"},u(s(e).copyrightTitle),9,I)):l("",!0)]))}});const T=S(B,[["__scopeId","data-v-4d63aa86"]]);export{T as _};
